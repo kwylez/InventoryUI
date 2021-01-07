348db56f0455005b93736b97f83fdef404fee552
Cory D. Wiles <corywiles@icloud.com>
2021-01-06 22:55:25 -0500

    Initial Commit

---
+++ InventoryUI/InventoryUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryUIApp.swift
+//  InventoryUI
+//
+//  Created by Cory D. Wiles on 1/6/21.
+//
+
+import SwiftUI
+
+@main
+struct InventoryUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
